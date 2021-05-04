13e80f61f7a60e05d4d51c14a93c35a035221857
Ricardo Cenci Fabris <ricardocencifabris@gmail.com>
2021-05-04 16:22:59 -0300

    Procurar ocorrencias de um subtexto em um texto

---
+++ SubsStringSearch.php
@@ -0,0 +1,48 @@
+<form method="GET">
+    <label for="needle">Needle</label>
+    <input type="text" name="needle" id="needle" value="lazy">
+    <label for="needle">Haystack</label>
+    <input type="text" name="haystack" id="haystack" value="The brown fox jumped over the lazy dog">
+    <input type="submit">
+</form>
+<?php
+// Um algoritmo deve buscar um sub-texto dentro de um texto fornecido e retornar a quantidade
+// de vezes que ele ocorre. (Não usar funções de busca em string). 
+
+if (!empty($_GET)) {
+    echo "There was ".search($_GET['needle'], $_GET['haystack'])." occurrences of the pattern ' {$_GET['needle']} ' in the text";
+}
+
+function search($needle, $haystack){
+    //A simple naive algorithm to search substrings
+    $haystackLen = strlen($haystack);
+    $buffer = "";
+    $occurrences = 0;
+    // $haystackLen - strlen($needle) -1 ignores the last latter becouse there is no way to match the needle there
+    for ($i=0; $i <= $haystackLen - strlen($needle); $i++) { 
+
+        if ($haystack[$i] == $needle[0]) {
+            //Check if the first char on the needle is the same as the haystack, if it is, iterate over the needle lenght to test if its a match
+            $j=0;
+            for (; $j < strlen($needle); $j++) { 
+
+                if ($haystack[$i + $j] == $needle[$j]) {
+                    //Stores is in a buffer to test later if the pattern is match
+                    $buffer = $buffer.$haystack[$i + $j];
+                }else{
+                    break;
+                }
+            }
+
+            $i = ($i+$j)-1;
+            if ($buffer == $needle) {
+                $occurrences++;
+            }
+            $buffer = "";
+
+        }
+    }
+    return $occurrences;
+}
+
+?>
